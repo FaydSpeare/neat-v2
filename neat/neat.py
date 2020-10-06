2f7c77e93b1fc56299aeeb5ccad28a7aa4aeb9aa
FaydSpear <fspea123@gmail.com>
2020-10-07 02:14:13 +1000

    fix indefinite loop

--- neat/neat.py
+++ neat/neat.py
@@ -11,11 +11,13 @@ class Neat:
         self.population = Population(num_inputs, num_outputs, organism_type, population_size)
 
 
-    def run(self, generations=100):
-        for i in range(generations):
+    def run(self, generations=None):
+        while True:
             if self.population.next(self.assessor_function):
                 print("\nSolver Organism/s found.\n")
                 return
+            if generations is not None and self.population.generation > generations:
+                break
 
 
     def get_solvers(self):
